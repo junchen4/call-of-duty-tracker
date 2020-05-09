0a08d3c7856d1a8af742d86069ac663697da9424
Jun Chen <junchen424@gmail.com>
2020-05-08 22:12:47 -0400

    Store initial data

--- app.js
+++ app.js
@@ -5,16 +5,42 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
 	const API = require('call-of-duty-api')();
-	API.login("username", "password").then(data => {
-		//I want Warzone Data
+	API.login("username", "pw").then(data => {
 		res.statusCode = 200;
-	  res.setHeader('Content-Type', 'text/plain');
-	  res.end('Logged in!');
+	  res.setHeader('Content-Type', 'application/json');
 
 		API.MWstats('jchizzle4').then(data => {
-		    console.log(data);  // see output
+			const jsonStringData = JSON.stringify(data);
+
+			// Transaction
+			const { Pool } = require('pg')
+			const pool = new Pool({
+			  user: 'junchen',
+			  host: 'localhost',
+			  database: 'callofduty',
+			  password: '',
+			  port: 5432,
+			})
+			;(async () => {
+			  // note: we don't try/catch this because if connecting throws an exception
+			  // we don't need to dispose of the client (it will be undefined)
+			  const client = await pool.connect()
+			  try {
+			    await client.query('BEGIN')
+			    const queryText = 'INSERT INTO raw_stats(id,data,download_time) VALUES($1,$2,$3) RETURNING id'
+			    const res = await client.query(queryText, [1, jsonStringData, new Date()])
+			    await client.query('COMMIT')
+			  } catch (e) {
+			    await client.query('ROLLBACK')
+			    throw e
+			  } finally {
+			    client.release()
+			  }
+			})().catch(e => console.error(e.stack))				
+
+			res.end(jsonStringData);
 		}).catch(err => {
-		    console.log(err);
+		  console.log(err);
 		});
 	}).catch(err => {
 	    console.log(err);
--- migrations/20200508043114-rawstats.js
+++ migrations/20200508043114-rawstats.js
@@ -1,30 +1,11 @@
-'use strict';
-
-var dbm;
-var type;
-var seed;
-
-/**
-  * We receive the dbmigrate dependency from dbmigrate initially.
-  * This enables us to not have to rely on NODE_PATH.
-  */
-exports.setup = function(options, seedLink) {
-  dbm = options.dbmigrate;
-  type = dbm.dataType;
-  seed = seedLink;
-};
-
 exports.up = function(db) {
-  db.createTable('raw_stats', {
+  return db.createTable('raw_stats', {
     id: { type: 'int', primaryKey: true },
-    data: 'json'
-  }, callback);
+    data: 'json',
+    download_time: 'timestamptz'
+  });
 };
 
 exports.down = function(db) {
-  db.dropTable('raw_stats', callback);
-};
-
-exports._meta = {
-  "version": 1
+  return db.dropTable('raw_stats');
 };
